fe4d0bee649b5043b2074603c2da8232ac48a8da
hosam mohamed <hosamelmalt@gmail.com>
2020-11-24 15:21:14 -0800

    Initial Commit

---
+++ Currency Exchange/Currency_ExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ExchangeApp.swift
+//  Currency Exchange
+//
+//  Created by Hossam on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
